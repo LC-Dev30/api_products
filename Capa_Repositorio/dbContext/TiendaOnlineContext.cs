3f07e5080bbea1cde1087b941548c06d412cbcb5
Leo <cristomiroca3005@gmail.com>
2023-08-25 08:36:54 -0400

    new changes

--- Capa_Repositorio/dbContext/TiendaOnlineContext.cs
+++ Capa_Repositorio/dbContext/TiendaOnlineContext.cs
@@ -21,10 +21,6 @@ public partial class TiendaOnlineContext : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Server=DIOSESAMOR\\SERVERLOCAL;Database=TiendaOnline;Trusted_Connection=True; Trust Server Certificate=true;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<CategoriasProducto>(entity =>
